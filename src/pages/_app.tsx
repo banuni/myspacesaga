01a8cc1395150c37537920bd707aef4ec93c750c
Nuni <nuni@kovrr.com>
2023-06-01 03:35:03 +0300

    ok

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
-import "~/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import Layout from "~/components/Layout";
